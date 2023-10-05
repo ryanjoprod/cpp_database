75ac934ff413336adb69f0c35ebfe5e935a9cc6a
Tech Head <techhead82@gmail.com>
2023-10-05 15:11:31 -0500

    Update SQL file to create database and table

--- fakeBank.sql
+++ fakeBank.sql
@@ -1,40 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Oct 05, 2023 at 04:19 PM
--- Server version: 10.11.3-MariaDB-1
--- PHP Version: 8.2.10
+CREATE DATABASE IF NOT EXISTS fakeBank;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fakeBank`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `BankAccount`
---
-
-CREATE TABLE `BankAccount` (
-  `accountNumber` int(11) NOT NULL,
-  `firstName` varchar(30) NOT NULL,
-  `lastName` varchar(30) NOT NULL,
-  `balance` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE fakeBank.bankAccount (
+  accountNumber INT(11) unsigned NOT NULL AUTO_INCREMENT,
+  firstName VARCHAR(11) NOT NULL,
+  lastName VARCHAR(11) NOT NULL,
+  balance DOUBLE NOT NULL
+)
